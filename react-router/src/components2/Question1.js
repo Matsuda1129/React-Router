a996f3f161ba72e0e6d703112470939f93e457eb
Matsuda1129 <mk1529ayukat315takoyaki@yahoo.co.jp>
2020-10-31 22:15:31 +0900

    develop2

--- react-router/src/components2/Question1.js
+++ react-router/src/components2/Question1.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Question1 = (props) => {
+
   return (
     <div>
-    <div style={{ color: "blue" }}>現在、生命保険に加入されていますか？</div>
-          <form>
-          <input type="radio" name = "select1"/>はい
-          <input type="radio" name = "select1"/>いいえ
-          </form>
+      <div style={{ color: "blue" }}>現在、生命保険に加入されていますか？</div>
+      <form>
+        <label>
+          <input type="radio" value="yes" name="value" checked={props.value === "yes"} onChange={props.onChange} />はい
+          <input type="radio" value="no" name="value" checked={props.value === "no"} onChange={props.onChange} />いいえ
+        </label>
+      </form>
     </div>
   )
 }
